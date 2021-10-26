e5d1a4dce84d672ec527b49613a7e7593d334f79
ni4yja <k2.liuba@gmail.com>
2021-10-26 23:34:17 +0300

    welcome page added

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="main-page">
+  <div class="main-page p-d-flex p-flex-column p-jc-center p-ai-center">
     <h1>KitKat</h1>
-    <p>Purrs: 19999</p>
-    <div class="cat-circle"></div>
+    <p>Purrs: {{ purrs }}</p>
+    <div @click="incrementPurrs" class="cat-circle"></div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      purrs: 0
+    }
+  },
+  methods: {
+    incrementPurrs () {
+      this.purrs += 1
+    }
+  }
+}
+</script>
 
 <style scoped>
   .cat-circle {
     width: 300px;
     height: 300px;
+    border-radius: 50%;
+    background: #efe92e;
   }
 </style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="welcome-page">
+    <h1>Welcome to Kittie Clicker!!!</h1>
+    <div>
+      <h2>Choose your cat</h2>
+      <img v-for="(kitten, i) in kittens" :src="kitten.src" :key="i" />
+    </div>
+    <div class="p-field">
+      <label for="name">Cat's Name</label>
+      <InputText v-model="catName" id="name" type="username" />
+    </div>
+    <Button label="Submit" class="p-button-outlined" />
   </div>
 </template>
 
 <script>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
 
 export default {
-  name: 'Welcome'
+  name: 'Welcome',
+  components: {
+    Button,
+    InputText
+  },
+  data () {
+    return {
+      catName: '',
+      kittens: [
+        {
+          src: require('../assets/kittie-1.jpg')
+        },
+        {
+          src: require('../assets/kittie-2.jpg')
+        },
+        {
+          src: require('../assets/kittie-3.jpg')
+        }
+      ]
+    }
+  }
 }
 </script>
+<style scoped>
+  img {
+    width: 300px;
+    height: 300px;
+  }
+</style>
