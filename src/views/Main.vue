b93e6e2bb84111287664de019f7a60a3cafdfc80
ni4yja <k2.liuba@gmail.com>
2021-10-26 16:36:01 +0300

    project setup

---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-page">
+    <h1>KitKat</h1>
+    <p>Purrs: 19999</p>
+    <div class="cat-circle"></div>
+  </div>
+</template>
+
+<style scoped>
+  .cat-circle {
+    width: 300px;
+    height: 300px;
+  }
+</style>
