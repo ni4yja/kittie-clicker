075355545210c45b5383e62d750729385a61df54
ni4yja <k2.liuba@gmail.com>
2021-10-28 17:18:04 +0300

    welcome page styled

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="welcome-page">
-    <h1>Welcome to Kittie Clicker!!!</h1>
-    <div>
-      <h2>Choose your cat</h2>
-      <img v-for="(kitten, i) in kittens" :src="kitten.src" :key="i" />
+  <div class="welcome-page p-grid">
+    <div class="p-col-8 p-offset-2">
+      <h1 class="p-mt-6">Welcome to Kittie Clicker!!!</h1>
+      <div>
+        <h2>Choose your kitten</h2>
+        <div class="p-mb-4">
+          <img
+            v-for="(kitten, i) in kittens"
+            @click="selectKitten(kitten)"
+            :src="kitten.src"
+            :key="i"
+            class="p-m-2"
+          />
+        </div>
+      </div>
+      <div class="p-field">
+        <label for="name" class="p-mr-3">Kitten's Name:</label>
+        <InputText v-model="kittenName" id="name" type="username" />
+      </div>
+      <Button label="Submit" class="p-button-outlined" />
     </div>
-    <div class="p-field">
-      <label for="name">Cat's Name</label>
-      <InputText v-model="catName" id="name" type="username" />
-    </div>
-    <Button label="Submit" class="p-button-outlined" />
   </div>
 </template>
 
@@ -25,7 +35,8 @@ export default {
   },
   data () {
     return {
-      catName: '',
+      kittenName: '',
+      kittenSrc: '',
       kittens: [
         {
           src: require('../assets/kittie-1.jpg')
@@ -38,12 +49,33 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    selectKitten (kitten) {
+      this.kittenSrc = kitten
+    }
   }
 }
 </script>
-<style scoped>
-  img {
-    width: 300px;
-    height: 300px;
-  }
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap');
+
+body,
+.p-button-label {
+  font-family: 'Nunito', sans-serif;
+}
+
+h1, h2, h3, h4 {
+  font-weight: 700;
+}
+
+.welcome-page img {
+  width: 250px;
+  height: 250px;
+}
+
+.welcome-page .p-inputtext {
+  border: 1px solid black;
+  width: 300px;
+}
 </style>
