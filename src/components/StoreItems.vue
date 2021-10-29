791c08fe321d6ef83f3a58d638d8fa3ca9c0d930
ni4yja <k2.liuba@gmail.com>
2021-10-29 17:17:35 +0300

    version one updated

---
+++ src/components/StoreItems.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Store</h2>
+  <ul>
+    <li v-for="item in items" :key="item.name">{{ item.name }} - {{ item.cost }}</li>
+  </ul>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['items'])
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 
+import items from '@/data/items'
+
 export default createStore({
   state: {
     kittenName: '',
@@ -15,14 +17,36 @@ export default createStore({
       {
         src: require('../assets/kittie-3.jpg')
       }
-    ]
+    ],
+    timer: null,
+    seconds: 0,
+    items,
+    purchasedItems: [],
+    user: {
+      level: 0
+    }
   },
   mutations: {
     updateKittenInfo (state, { field, value }) {
       state[field] = value
+    },
+    setTimer (state, payload) {
+      state.timer = payload
+    },
+    setSeconds (state, seconds) {
+      state.seconds = seconds
     }
   },
   actions: {
+    createTimer ({ commit, state }) {
+      const timer = setInterval(() => {
+        commit('setSeconds', state.seconds + 1)
+      }, 1000)
+      commit('setTimer', timer)
+    },
+    clearTimer ({ state }) {
+      clearInterval(state.timer)
+    }
   },
   modules: {
   },
